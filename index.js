a72c729f0b6a2e7118d29d53a7dd264d834c07f8
ferouz kassim <ferouzkassim@gmail.com>
2021-03-06 08:38:44 +0300

    new

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
-const navbar=document.getElementById('nav'),
-     heda=document.querySelector('header');
-const fnav=()=>{
-if(innerWidth<=760){
-navbar.children[0].style.display='none'
-}}
- 
-heda.onload=fnav
+const footer = document.getElementsByTagName('footer'),
+    header = document.getElementsByTagName('header'),
+    nav = document.getElementsByTagName('nav');
+const navbar = document.getElementById('nav'),
+    heda = document.querySelector('header');
+fnav = () => {
+    if (innerWidth <= 760) {
+        navbar.children[0].style.display = 'none'
+    }
+};
+domloader = (name) => {
+    nm = document.querySelector(`${name}`)
+    console.log('footer activer')
+    console.log(this.nm)
+}
+heda.onload = fnav
 heda.onload(fnav)
