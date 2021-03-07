d2e071b1044d7074d3fed4126822f7f2577af104
ferouz kassim <ferouzkassim@gmail.com>
2021-03-08 00:04:22 +0300

    aded mdi icons

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 const footer = document.getElementsByTagName('footer'),
     header = document.getElementsByTagName('header'),
-    nav = document.querySelector('nav');
-const navbar = document.getElementById('nav'),
+    nav = document.querySelector('nav'),
+    navbar = document.getElementById('nav'),
+    slider = document.createElement('i'),
     heda = document.querySelector('header');
 fnav = () => {
     if (innerWidth <= 760) {
-        navbar.children[0].style.display = 'none'
-        const slider = document.createElement('i')
+        navbar.children[0].style.display = 'none';
         nav.appendChild(slider)
-        slider.className = 'fas fa-menu-bar slider fa-3x'
+        slider.className = 'mdi mdi-menu mdi-48px'
+    } else {}
+};
+slider.onclick = () => {
+    if (this.style.display = 'none') {
+        alert('binf')
 
     }
-};
+}
 domloader = (name) => {
     nm = document.querySelector(`${name}`)
     console.log('footer activer')
